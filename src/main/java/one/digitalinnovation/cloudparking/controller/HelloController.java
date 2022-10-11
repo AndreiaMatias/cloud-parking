e65aff13e6fa325f293f8ab587af744539ae98d4
AndreiaMatias <deiamat@gmail.com>
2022-10-11 17:31:58 -0300

    projeto

---
+++ src/main/java/one/digitalinnovation/cloudparking/controller/HelloController.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package one.digitalinnovation.cloudparking.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author deiam
+ */
+@RestController
+@RequestMapping("/")
+public class HelloController {
+    
+    @GetMapping
+    public String hello(){
+        return "Hello DIO... Java";
+}
+    
+}
